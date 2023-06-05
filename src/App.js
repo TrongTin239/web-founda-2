2f8f3f31f46feb3d8707b84ce834957cd5438bb3
TrongTin <trongtin3311@gmail.com>
2023-06-05 16:35:56 +0700

    frist push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box from "@mui/material/Box";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const notify = () => toast("Wow so easy!");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box textAlign={"center"}> Task Manager</Box>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
     </div>
   );
 }
